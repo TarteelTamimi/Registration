157043f0b1d0bf28dcd01b5aa6e0878ef955db69
TarteelTamimi <TarteelTamimi@gmail.com>
2023-08-31 03:11:35 +0300

    updated

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import registerRouter from "./routers/register.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Server Up!");
+});
+
+app.use('/register', registerRouter);
+
+app.use((req, res) => {
+    res.status(404).send("You are asking for something I don't have :(")
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running and listening on port ${PORT}.`);
+});
